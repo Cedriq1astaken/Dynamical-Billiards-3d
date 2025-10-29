e29a5041f4c299b7955a7f37bcdeea935fb5b960
cedri <desinorcedric3@gmail.com>
2025-10-29 09:41:50 -0400

    Update main.jl

--- scripts/main.jl
+++ scripts/main.jl
@@ -5,7 +5,7 @@ include(srcdir("loader.jl"))
 cube = Cuboid([0.0, 0.0, 0.0], 1.0, 2.0, 1.0)
 tetrahedron = Tetrahedron([0.0, 0.0, 0.0], [[1.0, 1.0, 1.0], [1.0, -1.0, -1.0], [-1.0, 1.0, -1.0], [-1.0, -1.0, 1.0]])
 start = [0.0, 0.0, 0.0]
-velocity = [1.0, 2.115, -0.05]
+velocity = [1.0, 2.116, -0.05]
 
 p = Particle3D(copy(start), copy(velocity))
 println("Period: $(is_periodic(time_matrix(tetrahedron, p, 100)))")
